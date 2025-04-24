73b984e15335d26072247434ea8cfd884d1f020d
maty546 <mfusco@fi.uba.ar>
2025-04-24 01:25:38 -0300

    add repo mocks and test for payments service

--- secure_payment_service_challenge/go.mod
+++ secure_payment_service_challenge/go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hibiken/asynq v0.25.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
---
+++ secure_payment_service_challenge/service/secure_payments_test.go
@@ -0,0 +1,64 @@
+package service
+
+import (
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/maty546/secure_payment_service_challenge/models"
+	"github.com/maty546/secure_payment_service_challenge/repository/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSecurePaymentsService_StartTransfer(t *testing.T) {
+	testCtx, _ := gin.CreateTestContext(httptest.NewRecorder())
+	mockCtrl := gomock.NewController(t)
+	mockAccRepo := mocks.NewMockIAccountRepository(mockCtrl)
+	mockTrRepo := mocks.NewMockITransferRepository(mockCtrl)
+
+	service := NewService(mockAccRepo, mockTrRepo, "", "")
+
+	type mock struct {
+		times    int
+		response interface{}
+		err      error
+	}
+
+	type want struct {
+		expectedTransfer models.Transfer
+		expectedErr      error
+	}
+	nowTime := time.Now()
+	testTransfer := models.Transfer{ID: 1, Amount: 10, FromAccountID: "ext_a", ToAccountID: "b", CreatedAt: nowTime, UpdatedAt: nowTime}
+	testAcc := models.Account{ID: "b", Balance: 10}
+	expTransfer := models.Transfer{ID: 1, Amount: 10, FromAccountID: "ext_a", ToAccountID: "b", Status: models.TRANSFER_STATUS_PENDING, CreatedAt: nowTime, UpdatedAt: nowTime}
+
+	tests := []struct {
+		description string
+		transfer    models.Transfer
+		accMock     mock
+		trMock      mock
+		want        want
+	}{
+
+		{description: "happy path external to internal",
+			transfer: testTransfer,
+			accMock:  mock{times: 1, response: testAcc},
+			trMock:   mock{times: 1, response: expTransfer},
+			want:     want{expectedTransfer: expTransfer, expectedErr: nil}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			mockAccRepo.EXPECT().GetByID(gomock.Any(), "b").Return(tc.accMock.response, tc.accMock.err).Times(tc.accMock.times)
+			mockTrRepo.EXPECT().Save(gomock.Any(), expTransfer).Return(tc.trMock.response, tc.trMock.err).Times(tc.trMock.times)
+
+			resultTransfer, err := service.StartTransfer(testCtx, tc.transfer)
+
+			assert.Equal(t, tc.want.expectedTransfer, resultTransfer)
+			assert.Equal(t, tc.want.expectedErr, err)
+		})
+	}
+}
